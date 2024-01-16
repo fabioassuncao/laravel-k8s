59467ae28e2c62664d223a4c8048aa5918fcbbfa
Fábio Assunção <fabio23gt@gmail.com>
2024-01-16 16:37:24 -0300

    Disable Primary Key check for migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,10 @@ namespace App\Providers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Events\MigrationsEnded;
+use Illuminate\Database\Events\MigrationsStarted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +19,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // https://github.com/laravel/framework/issues/33238#issuecomment-897063577
+        Event::listen(MigrationsStarted::class, function () {
+            DB::statement('SET SESSION sql_require_primary_key=0');
+        });
+
+        Event::listen(MigrationsEnded::class, function () {
+            DB::statement('SET SESSION sql_require_primary_key=1');
+        });
     }
 
     /**
