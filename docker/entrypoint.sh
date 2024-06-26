89dd90e255837e24f6fff7e909b46b2a79f2b944
Fábio Assunção <fabio23gt@gmail.com>
2024-06-26 13:53:28 -0300

    Improvements

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,37 +17,40 @@ run_setup_tasks() {
     php artisan migrate --force || true
 }
 
-if [ -d "/var/www/html/vendor" ] ; then
-    # Run setup tasks if in automatic mode
-    if [ "$CONTAINER_MODE" = "automatic" ]; then
-        run_setup_tasks
-    fi
+# Check if vendor directory exists
+while [ ! -d "/var/www/html/vendor" ]; do
+    echo "WARNING: The directory /var/www/html/vendor does not exist yet. Please run the \"composer install\" command to ensure that all necessary dependencies are properly installed."
+    echo "Retrying in 180 seconds..."
+    sleep 180s
+done
 
-    # Execute role-specific commands
-    case "$CONTAINER_ROLE" in
-        app)
-            echo "INFO: Running octane..."
-            exec $ARTISAN octane:start --server=frankenphp --host=0.0.0.0 --port=8000
-            ;;
-        worker)
-            echo "INFO: Running the queue..."
-            exec $ARTISAN queue:work -vv --no-interaction --tries=3 --sleep=5 --timeout=300 --delay=10
-            ;;
-        horizon)
-            echo "INFO: Running the horizon..."
-            exec $ARTISAN horizon
-            ;;
-        scheduler)
-            while true; do
-                echo "INFO: Running scheduled tasks." && exec $ARTISAN schedule:run --verbose --no-interaction &
-                sleep 60s
-            done
-            ;;
-        *)
-            echo "Could not match the container role \"$CONTAINER_ROLE\""
-            exit 1
-            ;;
-    esac
-else
-    echo "WARNING: The directory /var/www/html/vendor does not exist yet. Please run the composer install command to ensure that all necessary dependencies are properly installed."
+# Run setup tasks if in automatic mode
+if [ "$CONTAINER_MODE" = "automatic" ]; then
+    run_setup_tasks
 fi
+
+# Execute role-specific commands
+case "$CONTAINER_ROLE" in
+    app)
+        echo "INFO: Running octane..."
+        exec $ARTISAN octane:start --host=0.0.0.0 --port=8000
+        ;;
+    worker)
+        echo "INFO: Running the queue..."
+        exec $ARTISAN queue:work -vv --no-interaction --tries=3 --sleep=5 --timeout=300 --delay=10
+        ;;
+    horizon)
+        echo "INFO: Running the horizon..."
+        exec $ARTISAN horizon
+        ;;
+    scheduler)
+        while true; do
+            echo "INFO: Running scheduled tasks." && exec $ARTISAN schedule:run --verbose --no-interaction &
+            sleep 60s
+        done
+        ;;
+    *)
+        echo "Could not match the container role \"$CONTAINER_ROLE\""
+        exit 1
+        ;;
+esac
